f296acd38f7e95e662eba6fe36e8de60799b955b
mohamed khairy <mohamedkhairy70@outlook.com>
2021-05-15 14:18:41 +0200

    Add project files.

---
+++ SWE.Data/SWEContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWE.Data
+{
+    public class SWEContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source = .\khairy;Initial catalog=SWEDB;Integrated Security=true");
+        }
+    }
+}
